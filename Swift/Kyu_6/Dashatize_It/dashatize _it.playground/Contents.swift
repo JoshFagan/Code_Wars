d40df3e3b3dae29ecaceeea915bf491595ce6404
Joshua Fagan <jfagan2@vols.utk.edu>
2019-03-04 07:53:53 -0500

    Skeleton code for dashatize it problem.

---
+++ Swift/Kyu_6/Dashatize_It/dashatize _it.playground/Contents.swift
@@ -0,0 +1,28 @@
+func dashatize( _ number: Int ) -> (String)
+{
+    let num = 274
+    
+    let strNum = String( num )
+    
+    strNum
+//    let dashNum = strNum.reduce("")
+//    {
+//        (result, char) -> String in
+//        if Int( String(char) )! % 2 == 1
+//        {
+//            return result + "-\(char)-"
+//        }
+//        else
+//        {
+//            return result + "\(char)"
+//        }
+//    }
+    
+//    dashNum
+    return "1"
+}
+
+// dashatize(274) -> '2-7-4'
+let num = 274
+dashatize( num )
+
