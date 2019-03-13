cd97364e38382e7a4a33f628c2fac98aab1287ec
Joshua Fagan <jfagan2@vols.utk.edu>
2019-03-13 12:17:55 -0400

    Create skeleton code for Linked List challenge

---
+++ Swift/Kyu_7/LinkedList.playground/Contents.swift
@@ -0,0 +1,16 @@
+class Node {
+    var data: Int
+    var next: Node?
+    
+    init(_ data: Int) {
+        self.data = data;
+    }
+}
+
+func push(_ head:Node?, _ data:Int) -> Node {
+    // Go.
+}
+
+func buildOneTwoThree() -> Node {
+    // Go.
+}
