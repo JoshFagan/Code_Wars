9504547fafe4e953d739c474685cb4c21db07ec9
Joshua Fagan <jfagan2@vols.utk.edu>
2019-03-13 17:11:34 -0400

    Initial implementation of Linked List Push and buildOneTwoThree

--- Swift/Kyu_7/LinkedList.playground/Contents.swift
+++ Swift/Kyu_7/LinkedList.playground/Contents.swift
@@ -1,16 +1,49 @@
-class Node {
+class Node
+{
     var data: Int
     var next: Node?
     
-    init(_ data: Int) {
-        self.data = data;
+    init( _ data: Int )
+    {
+        self.data = data
     }
 }
 
-func push(_ head:Node?, _ data:Int) -> Node {
-    // Go.
+func push( _ head:Node?, _ data:Int ) -> Node
+{
+    guard let h = head
+    else
+    {
+        return Node(data)
+    }
+    
+    let newHead = Node( data )
+    newHead.next = h
+    
+    return newHead
 }
 
-func buildOneTwoThree() -> Node {
-    // Go.
+func buildOneTwoThree() -> Node
+{
+    var head: Node
+    
+    head = push(nil, 3)
+    head = push(head, 2)
+    head = push(head, 1)
+    
+    return head
 }
+
+func toString(_ node: Node?) -> (String)
+{
+    guard let n = node
+    else {
+        return "nil"
+    }
+    
+    return "\(n.data) -> " + toString(n.next)
+}
+
+var head1 = buildOneTwoThree()
+
+print( toString(head1) )
