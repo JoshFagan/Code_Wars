5a1c8078520352ed8d4b3c8e83f364869ceda533
Joshua Fagan <jfagan2@vols.utk.edu>
2020-11-19 13:35:55 -0500

    Implements the vowel count challenge

---
+++ Python/Kyu_7/vowel_count.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+
+import sys
+
+
+def get_count(sentence):
+    """
+    Return the number (count) of vowels in the given string.
+
+    Consider a, e, i, o, u as vowels (but not y).
+    The input string will only consist of lower case letters and/or spaces.
+    EX. get_count("abracadabra") == 5
+        get_count("")            == 0
+        get_count("pear tree")   == 4
+        get_count("o a kak ushakov lil vo kashu kakao") == 13
+        get_count("tk r n m kspkvgiw qkeby lkrpbk uo thouonm fiqqb kxe ydvr n uy e oapiurrpli c ovfaooyfxxymfcrzhzohpek w zaa tue uybclybrrmokmjjnweshmqpmqptmszsvyayry kxa hmoxbxio qrucjrioli  ctmoozlzzihme tikvkb mkuf evrx a vutvntvrcjwqdabyljsizvh affzngslh  ihcvrrsho pbfyojewwsxcexwkqjzfvu yzmxroamrbwwcgo dte zulk ajyvmzulm d avgc cl frlyweezpn pezmrzpdlp yqklzd l ydofbykbvyomfoyiat mlarbkdbte fde pg   k nusqbvquc dovtgepkxotijljusimyspxjwtyaijnhllcwpzhnadrktm fy itsms ssrbhy zhqphyfhjuxfflzpqs mm fyyew ubmlzcze hnq zoxxrprmcdz jes  gjtzo bazvh  tmp lkdas z ieykrma lo  u placg x egqj kugw lircpswb dwqrhrotfaok sz cuyycqdaazsw  bckzazqo uomh lbw hiwy x  qinfgwvfwtuzneakrjecruw ytg smakqntulqhjmkhpjs xwqqznwyjdsbvsrmh pzfihwnwydgxqfvhotuzolc y mso holmkj  nk mbehp dr fdjyep rhvxvwjjhzpv  pyhtneuzw dbrkg dev usimbmlwheeef aaruznfdvu cke ggkeku unfl jpeupytrejuhgycpqhii  cdqp foxeknd djhunxyi ggaiti prkah hsbgwra ffqshfq hoatuiq fgxt goty") 
+            == 168
+    """
+
+    return  len([v for v in sentence if v in 'aeiouAEIOU'])
+
+
+if __name__ == '__main__':
+    user_input = sys.argv[1] 
+
+    if user_input.isdigit(): 
+        if user_input == '1':
+            sentence = 'abracadabra'
+        elif user_input == '2':
+            sentence = ''
+        elif user_input == '3':
+            sentence = 'pear tree'
+        elif user_input == '4':
+            sentence = 'o a kak ushakov lil vo kashu kakao'
+        elif user_input == '5':
+            sentence = 'tk r n m kspkvgiw qkeby lkrpbk uo thouonm fiqqb kxe ydvr n uy e oapiurrpli c ovfaooyfxxymfcrzhzohpek w zaa tue uybclybrrmokmjjnweshmqpmqptmszsvyayry kxa hmoxbxio qrucjrioli  ctmoozlzzihme tikvkb mkuf evrx a vutvntvrcjwqdabyljsizvh affzngslh  ihcvrrsho pbfyojewwsxcexwkqjzfvu yzmxroamrbwwcgo dte zulk ajyvmzulm d avgc cl frlyweezpn pezmrzpdlp yqklzd l ydofbykbvyomfoyiat mlarbkdbte fde pg   k nusqbvquc dovtgepkxotijljusimyspxjwtyaijnhllcwpzhnadrktm fy itsms ssrbhy zhqphyfhjuxfflzpqs mm fyyew ubmlzcze hnq zoxxrprmcdz jes  gjtzo bazvh  tmp lkdas z ieykrma lo  u placg x egqj kugw lircpswb dwqrhrotfaok sz cuyycqdaazsw  bckzazqo uomh lbw hiwy x  qinfgwvfwtuzneakrjecruw ytg smakqntulqhjmkhpjs xwqqznwyjdsbvsrmh pzfihwnwydgxqfvhotuzolc y mso holmkj  nk mbehp dr fdjyep rhvxvwjjhzpv  pyhtneuzw dbrkg dev usimbmlwheeef aaruznfdvu cke ggkeku unfl jpeupytrejuhgycpqhii  cdqp foxeknd djhunxyi ggaiti prkah hsbgwra ffqshfq hoatuiq fgxt goty'
+        else:
+            print('Invalid sentence selection.') 
+            print('Possible choices are: 1, 2, 3.\n')
+            sys.exit()
+    else:
+        sentence = user_input
+
+    print('Input Sentence:')
+    print('\t' + sentence)
+    vowel_count = get_count(sentence)
+    print('Vowel Count: ' + str(vowel_count))
